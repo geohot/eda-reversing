d5bdd07e382d7ec18c5e269bd84e4c7d955550ac
George Hotz <geohot@gmail.com>
2009-04-14 18:33:06 -0400

    merged

---
+++ View/ConsoleFrontEnd.cc
@@ -0,0 +1,7 @@
+// ConsoleFrontEnd.cc -- Apr 14, 2009
+//    by geohot
+//  released under GPLv3, see http://gplv3.fsf.org/
+
+//this gets passed a pointer to the model, read only
+//and a pointer to the core mailbox and the model mailbox
+
